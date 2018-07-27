b6db9fdde09c39b3a5e5c7122b7ad99020937568
Ron van den Enden <endenron@gmail.com>
2018-07-27 16:15:20 +0200

    Reclyce view ingebouwd, met dank aan Steven

--- app/src/main/java/com/example/myveryfirstapp/view/MainActivity.java
+++ app/src/main/java/com/example/myveryfirstapp/view/MainActivity.java
@@ -2,45 +2,44 @@ package com.example.myveryfirstapp.view;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+
 
 import com.example.myveryfirstapp.R;
 import com.example.myveryfirstapp.domain.Melding;
 import com.example.myveryfirstapp.viewmodel.MeldingViewModel;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public static final String INFO_MELDING = "com.example.myfirstapp.MESSAGE";
 
+    // Recycler view
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     // tbv MeldingView
     private MeldingViewModel viewModel = null;
 
-
-
-    // tbv Firedatabase
-//    private DatabaseReference valueRef;
-//    private ValueEventListener valueListener;
+    // logging
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Create a ViewModel the first time the system calls an activity's onCreate() method.
         // Re-created activities receive the same MyViewModel instance created by the first activity.
@@ -49,22 +48,14 @@ public class MainActivity extends AppCompatActivity {
 
         // attach an observer to the viewmodel methodview
         Log.v(TAG, "Register Observer for viewModel method");
+
         viewModel.haalMeldingen().observe(this, new Observer<List<Melding>>() {
             @Override
-            // When data is changed in viewmodel: update ui
-            public void onChanged(@Nullable List<Melding> lijstMeldingen) {
-
-                String inhoudTextView = "";
-                for (Melding melding : lijstMeldingen) {
-                    // verwerk één melding
-                    inhoudTextView = inhoudTextView + melding.getTekst() + "\n";
-
-                }
-                TextView textView = findViewById(R.id.messages);
-                textView.setText(inhoudTextView);
+            public void onChanged(@Nullable List<Melding> meldings) {
+                initMeldingRecyclerView(meldings);
             }
-
         });
+
     }
 
     public void maakMelding (View view){
@@ -83,6 +74,14 @@ public class MainActivity extends AppCompatActivity {
         viewModel.voegMeldingToe(nieuweMelding);
 
 
+    }
+
+
+    private void initMeldingRecyclerView(List<Melding> lijstMeldingen) {
+        RecyclerView recyclerView = findViewById(R.id.meldingen_recycler_view);
+        MeldingRecyclerViewAdapter adapter = new MeldingRecyclerViewAdapter(lijstMeldingen, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
---
+++ app/src/main/java/com/example/myveryfirstapp/view/MeldingRecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package com.example.myveryfirstapp.view;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myveryfirstapp.R;
+import com.example.myveryfirstapp.domain.Melding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeldingRecyclerViewAdapter extends RecyclerView.Adapter<MeldingRecyclerViewAdapter.MeldingDetailViewHolder> {
+
+    private static final String TAG = "MeldingRecViewAdapter";
+
+
+    private List<Melding> mlijstMeldingen;
+
+    private Context mContext;
+
+    public MeldingRecyclerViewAdapter(List<Melding> lijstMeldingen, Context mContext) {
+        this.mlijstMeldingen = lijstMeldingen;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public MeldingDetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.melding_lijst_item, parent, false);
+        MeldingDetailViewHolder holder = new MeldingDetailViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MeldingDetailViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+        holder.meldingTekst.setText(mlijstMeldingen.get(position).getTekst());
+        holder.meldingGebruiker.setText(mlijstMeldingen.get(position).getGebruiker());
+        holder.meldingDatumTijd.setText(mlijstMeldingen.get(position).getDatumTijd());
+
+        holder.meldingenOntvangerParentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on" + mlijstMeldingen.get(position));
+
+                Toast.makeText(mContext, mlijstMeldingen.get(position).getTekst(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mlijstMeldingen.size();
+    }
+
+    public class MeldingDetailViewHolder extends RecyclerView.ViewHolder {
+
+        TextView meldingTekst;
+        TextView meldingGebruiker;
+        TextView meldingDatumTijd;
+        ConstraintLayout meldingenOntvangerParentLayout;
+
+        public MeldingDetailViewHolder(View itemView) {
+            super(itemView);
+            meldingTekst = itemView.findViewById(R.id.melding_tekst);
+            meldingGebruiker = itemView.findViewById(R.id.melding_gebruiker);
+            meldingDatumTijd = itemView.findViewById(R.id.melding_datumTijd);
+            meldingenOntvangerParentLayout = itemView.findViewById(R.id.meldingen_ontvanger_parent_layout);
+        }
+    }
+}
